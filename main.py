9c0defd77b9e268689e5a64d56e7195c874059a5
shakedlokits <shaked.lokits@gmail.com>
2019-05-23 14:50:27 +0300

    NameCollector: Second task.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 page = """
@@ -18,8 +18,18 @@ page = """
 # http://nowthatsnifty.blogspot.com/2010/05/list-of-prank-names.html :)
 names = ["Al Gore-Rhythm", "Amy Stake", "Cal Culator"]
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def get_names():
+    return page % ", ".join(names)
+
+
+@app.route('/', methods=["POST"])
+def add_name():
+    new_name = request.form["name"].strip()
+    if new_name:
+        names.append(new_name)
+
     return page % ", ".join(names)
 
 app.run(port=3000)
